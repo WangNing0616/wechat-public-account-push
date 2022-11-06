894e797da5fd870073d8d40ebf1e6e199504e181
WangNing0616 <101250702+WangNing0616@users.noreply.github.com>
2022-11-06 22:18:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ export const config = {
     // 模板消息id
     templateId: "uyyKR2NGMeNe269cbfLl6K32_PIsYDxpaber_sjacSs",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: ["o6Kdj6FbIe3OrOZPTtgYb2F_KLpc","o6Kdj6BEshp_J_axXCOYytua6Jn4"],
      
     // 信息配置
     // 所在省份
